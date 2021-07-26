61280872e48608c3950b96f0909b586af4f831f4
Richa-Dits <87110521+Richa-Dits@users.noreply.github.com>
2021-07-26 18:22:01 +0530

    test

---
+++ src/solutions/FieldMgt.Repository/Repository/MockRepository/MockContactDetailRepository.cs
@@ -0,0 +1,30 @@
+﻿using FieldMgt.Core.Interfaces;
+using System.Collections.Generic;
+using FieldMgt.Core.DomainModels;
+using FieldMgt.Core.Interfaces.MockRepository;
+
+namespace FieldMgt.Repository.Repository.MockRepository
+{
+    public class MockContactDetailRepository:IMockContactDetailsRepository
+    {
+        private readonly List<ContactDetail> _contactDetail;
+        public MockContactDetailRepository()
+        {
+            IEnumerable<ContactDetail> _contactDetail = new List<ContactDetail>()
+            {
+                new ContactDetail() { ContactDetailId = 1,
+                    PrimaryEmail = "abc@abc.com", AlternateEmail="def@def.com", PrimaryPhone ="9876542345",AlternatePhone="1234456879" },
+                 new ContactDetail() { ContactDetailId = 2,
+                    PrimaryEmail = "xyz@abc.com", AlternateEmail="qwe@def.com", PrimaryPhone ="7876542345",AlternatePhone="7834456879" }, 
+                new ContactDetail() { ContactDetailId = 3,
+                    PrimaryEmail = "pqr@abc.com", AlternateEmail="rty@def.com", PrimaryPhone ="9976542345",AlternatePhone="9034456879" },
+                 new ContactDetail() { ContactDetailId = 4,
+                    PrimaryEmail = "lmn@abc.com", AlternateEmail="uio@def.com", PrimaryPhone ="9776542345",AlternatePhone="6734456879" },
+            };
+        }
+        public IEnumerable<ContactDetail> GetAllItems()
+        {
+            return _contactDetail;
+        }
+    }
+}
---
+++ src/tests/Vendor/Respositories/TestProject1/GetContactDetailTestCase.cs
@@ -0,0 +1,29 @@
+﻿using FieldMgt.API.Controllers;
+using FieldMgt.Core.Interfaces.MockRepository;
+using FieldMgt.Repository.Repository;
+using FieldMgt.Repository.Repository.MockRepository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using Xunit;
+
+namespace TestProject1
+{
+    public class GetContactDetailTestCase
+    {
+        protected readonly MockContactDetailController _controller;
+        protected readonly IMockContactDetailsRepository _service;
+        public GetContactDetailTestCase()
+        {
+            _service = new MockContactDetailRepository();
+            _controller = new MockContactDetailController(_service);
+        }
+        [Fact]
+        public void GetContacts_ShouldReturnOkResult()
+        {
+            // Act
+            var okResult = _controller.Get();
+            // Assert
+            Assert.IsType<OkObjectResult>(okResult.Result);
+        }
+    }
+}
