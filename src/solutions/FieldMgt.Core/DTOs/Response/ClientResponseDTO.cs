f9b6c48c6a22be126d2f3c7cb8964c53914db484
NehaDits <neha.thakur@ditstek.com>
2021-08-02 13:25:09 +0530

    commit DTOs/Response/ClientResponseDTO.cs

---
+++ src/solutions/FieldMgt.Core/DTOs/Response/ClientResponseDTO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FieldMgt.Core.DTOs.Response
+{
+    public class ClientResponseDTO
+    {
+        public int ClientId { get; set; }
+        public string ClienyCompanyName { get; set; }
+        public string ClientDescription { get; set; }
+        public int? ClientSource { get; set; }
+        public string PermanentAddress { get; set; }
+        public int PermanentCity { get; set; }
+        public int PermanentState { get; set; }
+        public int PermanentCountry { get; set; }
+        public string PermanentZipCode { get; set; }
+        public string CorrespondenceAddress { get; set; }
+        public int CorrespondenceCity { get; set; }
+        public int CorrespondenceState { get; set; }
+        public int CorrespondenceCountry { get; set; }
+        public string CorrespondenceZipCode { get; set; }
+        public string PrimaryPhone { get; set; }
+        public string AlternatePhone { get; set; }
+        public string PrimaryEmail { get; set; }
+        public string AlternateEmail { get; set; }
+        public bool IsActive { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime? CreatedOn { get; set; }
+    }
+}
